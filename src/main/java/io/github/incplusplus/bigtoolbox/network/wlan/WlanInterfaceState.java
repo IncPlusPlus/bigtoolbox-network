aa8de2633ca7ecbcf7103b8cab52b35461e6fc45
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-03-07 15:30:12 -0500

    Added WlanInterfaceState

---
+++ src/main/java/io/github/incplusplus/bigtoolbox/network/wlan/WlanInterfaceState.java
@@ -0,0 +1,5 @@
+package io.github.incplusplus.bigtoolbox.network.wlan;
+
+public enum WlanInterfaceState {
+	NotReady, Connected, AdHocNetworkFormed, Disconnecting, Disconnected, Associating, Discovering, Authenticating
+}

9befd3929eb7cab6d5895ca8ac7472d415078094
IncPlusPlus <cloherty.ryan@gmail.com>
2020-04-04 19:12:02 -0400

    Added several NetworkManager classes

---
+++ src/main/java/org/freedesktop/networkmanager/device/Wired.java
@@ -0,0 +1,23 @@
+package org.freedesktop.networkmanager.device;
+
+import org.freedesktop.dbus.annotations.DBusInterfaceName;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.dbus.interfaces.DBusInterface;
+import org.freedesktop.dbus.messages.DBusSignal;
+import org.freedesktop.dbus.types.Variant;
+
+import java.util.Map;
+
+@DBusInterfaceName("org.freedesktop.NetworkManager.Device.DeviceEthernet")
+public interface Wired extends DBusInterface
+{
+	public static class PropertiesChanged extends DBusSignal
+	{
+		public final Map<CharSequence,Variant<?>> properties;
+		public PropertiesChanged(String path, Map<CharSequence,Variant<?>> properties) throws DBusException
+		{
+			super(path, properties);
+			this.properties = properties;
+		}
+	}
+}

c2dd57b3f6ddd5f7438a017263f9f205d0d869c7
IncPlusPlus <cloherty.ryan@gmail.com>
2020-04-04 18:07:00 -0400

    Start adding some dbus-related code

---
+++ src/main/java/io/github/incplusplus/bigtoolbox/network/wlan/interop/lin/dbushelpers/systemd1/Manager.java
@@ -0,0 +1,22 @@
+package io.github.incplusplus.bigtoolbox.network.wlan.interop.lin.dbushelpers.systemd1;
+
+import org.freedesktop.dbus.DBusPath;
+import org.freedesktop.dbus.annotations.DBusInterfaceName;
+import org.freedesktop.dbus.interfaces.DBusInterface;
+import org.freedesktop.dbus.interfaces.Properties;
+
+@SuppressWarnings("SpellCheckingInspection")
+@DBusInterfaceName("org.freedesktop.systemd1.Manager")
+public interface Manager extends DBusInterface, Properties {
+	public CharSequence Dump();
+	
+	/**
+	 * Get a systemd Unit of a certain name.
+	 * For example, the call GetUnit("NetworkManager.service") returns
+	 * the path "/org/freedesktop/systemd1/unit/NetworkManager_2eservice"
+	 *
+	 * @param unitName the name of the unit
+	 * @return the dbus path to the object
+	 */
+	DBusPath GetUnit(CharSequence unitName);
+}

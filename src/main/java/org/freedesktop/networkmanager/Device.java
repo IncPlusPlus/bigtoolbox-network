83cc80b401352d926f872888745b1ec469e35944
IncPlusPlus <cloherty.ryan@gmail.com>
2020-04-30 22:42:20 -0400

    Some cleaning

--- src/main/java/org/freedesktop/networkmanager/Device.java
+++ src/main/java/org/freedesktop/networkmanager/Device.java
@@ -4,6 +4,7 @@ import org.freedesktop.Pair;
 import org.freedesktop.dbus.annotations.DBusInterfaceName;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.DBusInterface;
+import org.freedesktop.dbus.interfaces.Properties;
 import org.freedesktop.dbus.messages.DBusSignal;
 import org.freedesktop.dbus.types.UInt32;
 import org.freedesktop.dbus.types.UInt64;
@@ -12,24 +13,12 @@ import org.freedesktop.dbus.types.Variant;
 import java.util.Map;
 
 @DBusInterfaceName("org.freedesktop.NetworkManager.Device")
-public interface Device extends DBusInterface
-{
-	public static class StateChanged extends DBusSignal
-	{
-		public final UInt32 new_state;
-		public final UInt32 old_state;
-		public final UInt32 reason;
-		public StateChanged(String path, UInt32 new_state, UInt32 old_state, UInt32 reason) throws DBusException
-		{
-			super(path, new_state, old_state, reason);
-			this.new_state = new_state;
-			this.old_state = old_state;
-			this.reason = reason;
-		}
-	}
-	
+public interface Device extends DBusInterface, Properties {
 	public void Reapply(Map<CharSequence, Map<CharSequence, Variant<?>>> connection, UInt64 version_id, UInt32 flags);
-	public Pair<Map<CharSequence,Map<CharSequence,Variant<?>>>, UInt64> GetAppliedConnection(UInt32 flags);
+	
+	public Pair<Map<CharSequence, Map<CharSequence, Variant<?>>>, UInt64> GetAppliedConnection(UInt32 flags);
+	
 	public void Disconnect();
+	
 	public void Delete();
 }
--- src/main/java/org/freedesktop/networkmanager/device/Wireless.java
+++ src/main/java/org/freedesktop/networkmanager/device/Wireless.java
@@ -4,6 +4,7 @@ import org.freedesktop.dbus.DBusPath;
 import org.freedesktop.dbus.annotations.DBusInterfaceName;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.DBusInterface;
+import org.freedesktop.dbus.interfaces.Properties;
 import org.freedesktop.dbus.messages.DBusSignal;
 import org.freedesktop.dbus.types.Variant;
 
@@ -14,29 +15,12 @@ import java.util.Map;
  * Auto-generated class.
  */
 @DBusInterfaceName("org.freedesktop.NetworkManager.Device.Wireless")
-public interface Wireless extends DBusInterface {
-	
+public interface Wireless extends DBusInterface, Properties {
 	public List<DBusPath> GetAccessPoints();
 	
 	public List<DBusPath> GetAllAccessPoints();
 	
 	public void RequestScan(Map<String, Variant<?>> options);
-	
-	public static class PropertiesChanged extends DBusSignal {
-		
-		private final Map<String, Variant<?>> properties;
-		
-		PropertiesChanged(String _path, String _interfaceName, Map<String, Variant<?>> _properties)
-				throws DBusException {
-			super(_path, _interfaceName);
-			this.properties = _properties;
-		}
-		
-		public Map<String, Variant<?>> getProperties() {
-			return properties;
-		}
-		
-	}
 	
 	public static class AccessPointAdded extends DBusSignal {
 		
